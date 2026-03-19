1c9285762054731216faaebba3ee6e297506e26d
Radu Racariu <radu.racariu.ext@siemens.com>
2026-03-19 08:53:29 +0200

    Improve String andRef types

--- src/haystack/val/string.rs
+++ src/haystack/val/string.rs
@@ -2,6 +2,9 @@
 
 //! Haystack Str
 
+use core::fmt;
+use std::ops::Deref;
+
 use crate::haystack::val::Value;
 
 /// Haystack `Str`
@@ -41,11 +44,18 @@ impl Str {
     }
 }
 
+// Make a Haystack `Str` from a `String`
+impl From<String> for Str {
+    fn from(value: String) -> Self {
+        Str { value }
+    }
+}
+
 // Make a Haystack `Str` from a `&str` slice
 impl From<&str> for Str {
     fn from(value: &str) -> Self {
         Str {
-            value: value.into(),
+            value: value.to_owned(),
         }
     }
 }
@@ -83,5 +93,55 @@ impl TryFrom<&Value> for Str {
             Value::Str(v) => Ok(v.clone()),
             _ => Err("Value is not an `Str`"),
         }
+    }
+}
+/// Formats the `Str` using its inner `String`'s `Display` implementation
+impl fmt::Display for Str {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+/// Dereferences `Str` to `str`, making all `str` methods available on `&Str`
+/// and allowing `&Str` to be used where `&str` is expected.
+impl Deref for Str {
+    type Target = str;
+    fn deref(&self) -> &str {
+        &self.value
+    }
+}
+
+/// Allows `Str` to be used with generic APIs that accept `impl AsRef<str>`
+impl AsRef<str> for Str {
+    fn as_ref(&self) -> &str {
+        &self.value
+    }
+}
+
+/// Extracts the owned `String` from a `Str`
+impl From<Str> for String {
+    fn from(s: Str) -> String {
+        s.value
+    }
+}
+
+/// Converts an owned `String` directly to a `Str` `Value`
+impl From<String> for Value {
+    fn from(value: String) -> Self {
+        Value::Str(value.into())
+    }
+}
+
+/// Allows comparing `Str` with `str` directly: `some_str == "foo"`
+impl PartialEq<str> for Str {
+    fn eq(&self, other: &str) -> bool {
+        self.value == other
+    }
+}
+
+/// Allows comparing `Str` with `String` directly: `some_str == owned`
+impl PartialEq<String> for Str {
+    fn eq(&self, other: &String) -> bool {
+        self.value == *other
     }
 }
--- tests/values/test_string.rs
+++ tests/values/test_string.rs
@@ -22,3 +22,66 @@ fn test_ref_from() {
     let str = Str::from("id");
     assert_eq!(str.value, "id".to_string());
 }
+
+#[test]
+fn test_ref_from_string() {
+    let str = Str::from("id".to_string());
+    assert_eq!(str.value, "id".to_string());
+}
+
+#[test]
+fn test_str_as_str() {
+    let str = Str::make("foo");
+    assert_eq!(str.as_str(), "foo");
+}
+
+#[test]
+fn test_str_display() {
+    let str = Str::make("foo");
+    assert_eq!(format!("{}", str), "foo");
+}
+
+#[test]
+fn test_str_deref() {
+    let str = Str::make("hello world");
+    // &Str coerces to &str; all str methods are available
+    assert_eq!(str.len(), 11);
+    assert!(str.contains("world"));
+    assert!(str.starts_with("hello"));
+}
+
+#[test]
+fn test_str_as_ref() {
+    fn takes_as_ref(s: impl AsRef<str>) -> usize {
+        s.as_ref().len()
+    }
+    assert_eq!(takes_as_ref(Str::make("foo")), 3);
+}
+
+#[test]
+fn test_str_into_string() {
+    let str = Str::make("foo");
+    let owned: String = str.into();
+    assert_eq!(owned, "foo");
+}
+
+#[test]
+fn test_string_into_value() {
+    let value = Value::from(String::from("foo"));
+    assert!(value.is_str());
+    assert_eq!(Str::try_from(&value), Ok(Str::make("foo")));
+}
+
+#[test]
+fn test_str_partial_eq_str_slice() {
+    let str = Str::make("foo");
+    assert_eq!(str, *"foo");
+    assert_ne!(str, *"bar");
+}
+
+#[test]
+fn test_str_partial_eq_string() {
+    let str = Str::make("foo");
+    assert_eq!(str, String::from("foo"));
+    assert_ne!(str, String::from("bar"));
+}
